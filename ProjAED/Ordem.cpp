8c08ca5be9ab634348b516bd6a9b0b9a5494a8c0
HeckRodSav <hrs.heitor@hotmail.com.br>
2017-04-08 21:24:09 -0300

    0.3 - The Ordem Class and The QuickSort

---
+++ ProjAED/Ordem.cpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace ED
+{
+	template <typename Tipo> class Ordem
+	{
+		static void QuickSort(Tipo* vet, int length)
+		{
+			Tipo aux;
+			int i = 0, j = 0;
+			for (j = 1; j < length; j++) {
+				aux = vet[j];
+				i = j - 1;
+				while ((i >= 0) && (vet[i] > aux)) {
+					vet[i + 1] = vet[i];
+					i--;
+				}
+				vet[i + 1] = aux;
+			}
+		}
+	};
+}
