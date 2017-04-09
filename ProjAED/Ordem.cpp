20e9eff74eb21f588593f78841173af32e0477d0
HeckRodSav <hrs.heitor@hotmail.com.br>
2017-04-08 21:51:38 -0300

    0.4 - The First Case and The Random Vector

--- ProjAED/Ordem.cpp
+++ ProjAED/Ordem.cpp
@@ -4,14 +4,17 @@ namespace ED
 {
 	template <typename Tipo> class Ordem
 	{
+	public:
 		static void QuickSort(Tipo* vet, int length)
 		{
 			Tipo aux;
 			int i = 0, j = 0;
-			for (j = 1; j < length; j++) {
+			for (j = 1; j < length; j++)
+			{
 				aux = vet[j];
 				i = j - 1;
-				while ((i >= 0) && (vet[i] > aux)) {
+				while ((i >= 0) && (vet[i] > aux))
+				{
 					vet[i + 1] = vet[i];
 					i--;
 				}
--- ProjAED/ProjAED.cpp
+++ ProjAED/ProjAED.cpp
@@ -8,16 +8,22 @@
 
 using namespace std;
 
-unsigned int seeed[] = { 4,81,151,1601,2307,4207 };
-unsigned long int sizee[] = { 1000,30000,90000,270000,810000,243000,7290000,21870000,65610000 };
-string tipe_name[] = { "heapsort", "quicksort", "Mergesort", "S c++", "Qs C" };
-unsigned int time_name_size = 5;
-
 typedef unsigned int ui;
+typedef unsigned long int uli;
+
+ui seeed[] = { 4, 81, 151, 1601, 2307, 4207 };
+//uli sizee[] = { 1000,30000,90000,270000,810000,243000,7290000,21870000,65610000 };
+uli sizee[] = { 10, 30, 90, 270, 810, 2430, 729, 2187, 6561 };
+string tipe_name[] = { "heapsort", "quicksort", "Mergesort", "S c++", "Qs C" };
+ui time_name_size = 5;
 
 int main()
 {
 	double start = 0, end = 0;
+	int *V = new int[sizee[8]];
+
+	uniform_int_distribution<int32_t> seq;
+	mt19937 rnd;
 
 	for (ui i = 0; i < time_name_size; i++) //Select algorithm
 	{
@@ -27,27 +33,31 @@ int main()
 
 		for (ui j = 0; j < sizeof(sizee) / sizeof(sizee[0]); j++) //Select size of vector
 		{
+			for (uli l = 0; l < sizee[j]; l++) V[l] = 0; //Clean the vector
+
 			cout << "size: " << setfill(' ') << setw(9) << left << sizee[j] << ": "; //Show size
 
 			for (ui k = 0; k < sizeof(seeed) / sizeof(seeed[0]); k++) //Select current seed
 			{
+				rnd.seed(seeed[k]); //Set random seed
+				for (uli l = 0; l < sizee[j]; l++) V[l] = seq(rnd); //Set random vector
 
 				start = clock();
 				switch (i)
 				{
-				case 0:
+				case 0:	//HeapSort
 					//cout << 'a';
 					break;
-				case 1:
-					//cout << 'b';
+				case 1: //QuickSort
+					ED::Ordem<int>::QuickSort(V, sizee[j]);
 					break;
-				case 2:
+				case 2:	//MergeSort
 					//cout << 'c';
 					break;
-				case 3:
+				case 3:	//S C++
 					//cout << 'd';
 					break;
-				case 4:
+				case 4: //Qs C
 					//cout << 'e';
 					break;
 				}
